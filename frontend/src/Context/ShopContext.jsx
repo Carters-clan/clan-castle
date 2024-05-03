f94dd928673d8b7653042cd14c040f5822f5e3f0
Carters-clan <carterrsclan@gmail.com>
2024-05-03 18:57:35 +0530

    Update Item component to include onClick for scrolling to top, add addToCart function to ShopContext and use it in ProductDisplay component, add RelatedProducts component to Product page

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -2,11 +2,28 @@ import React, { createContext } from "react";
 import all_product from '../Components/Assets/all_product';
 
 export const ShopContext = createContext(null);
+    const getDefaultCart = () => {
+      let cart = {};
+      for (let index = 0; index < all_product.length+1; index++) {
+        cart[index] = 0;
+      }
+      return cart;
+    };
 
 const ShopContextProvider = (props) => {
     
-    const contextValue = {all_product};
+    const [cartItems, setCartItems] = React.useState(getDefaultCart());
     
+    const addToCart = (itemID) => {
+        setCartItems((prev) => ({...prev, [itemID]: prev[itemID] + 1}));
+        console.log(cartItems);
+    }
+    const removeFromCart = (itemID) => {
+      setCartItems((prev) => ({ ...prev, [itemID]: prev[itemID] - 1 }));
+    };
+    
+    const contextValue = { all_product, cartItems, addToCart, removeFromCart };
+
     return (
         <ShopContext.Provider value={contextValue}>
             {props.children}
